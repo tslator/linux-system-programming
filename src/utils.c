9d9b69fc736b159760c2e3c067cb058b1bac7fcc
Tim Slator <trslator@gmail.com>
2026-02-22 18:30:46 +0000

    Refactor create_file function to use Context struct for permissions and mode; remove unused file creation functions

--- src/utils.c
+++ src/utils.c
@@ -5,9 +5,9 @@
 
 #include "utils.h"
 
-static char *create_file_with_permissions(const char *filename, int permissions, mode_t mode)
+char *create_file(const char *filename, Context context)
 {
-    int fd = open(filename, permissions, mode);
+    int fd = open(filename, context.permissions, context.mode);
     if (fd == -1) 
     {
         perror("Failed to create file");
@@ -16,31 +16,6 @@ static char *create_file_with_permissions(const char *filename, int permissions,
 
     close(fd);
     return strdup(filename);
-}
-
-// char *create_readonly_file(const char *filename)
-// {
-//     return create_file_with_permissions(filename, O_CREAT | O_RDONLY, 0644);
-// }
-
-// char *create_readwrite_file(const char *filename)
-// {
-//     return create_file_with_permissions(filename, O_CREAT | O_RDWR, 0644);
-// }
-
-// char *create_writeonly_file(const char *filename)
-// {
-//     return create_file_with_permissions(filename, O_CREAT | O_WRONLY, 0644);
-// }
-
-// char *create_executable_file(const char *filename)
-// {
-//     return create_file_with_permissions(filename, O_CREAT | O_RDWR, 0755);
-// }
-
-char *create_file(const char *filename, Context context)
-{
-    return create_file_with_permissions(filename, context.permissions, context.mode);
 }
 
 void remove_file(const char *filename)
--- src/utils.h
+++ src/utils.h
@@ -7,11 +7,6 @@ typedef struct {
     mode_t mode;
 } Context;
 
-// char *create_readonly_file(const char *filename);
-// char *create_readwrite_file(const char *filename);
-// char *create_writeonly_file(const char *filename);
-// char *create_executable_file(const char *filename);
-
 char *create_file(const char *filename, Context context);
 void remove_file(const char *filename);
 
